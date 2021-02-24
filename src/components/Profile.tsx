ea5fb0a828ccdf05542a60cb4bab67f801a3baaf
Gabriel Mason <g.masson010@gmail.com>
2021-02-24 09:55:11 -0300

    feat: Adds level to profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useChallenges } from '../hooks/challenges';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile(): JSX.Element {
+  const { level } = useChallenges();
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/gmass0n.png" alt="Gabriel Masson" />
@@ -11,7 +14,7 @@ export function Profile(): JSX.Element {
         <p>
           <img src="icons/level.svg" alt="Level"/>  
           
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
